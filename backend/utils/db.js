66e649c547bfa544d2e49e39959583ccb4abce75
student <jayprakashmohanta55@gmail.com>
2025-03-31 01:37:06 +0530

    ready for deployment

---
+++ backend/utils/db.js
@@ -0,0 +1,14 @@
+
+
+//connection of data bases links------------
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('mongodb connected successfully.');
+    } catch (error) {
+        console.log(error);
+    }
+}
+export default connectDB;
